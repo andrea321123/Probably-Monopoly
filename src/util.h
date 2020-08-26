7b0f846a7ab44019202ad80da5a4eb9daa77dcc1
andrea321123 <gal.andrea.van@gmail.com>
2020-08-26 11:22:33 +0200

    Add community chest and chance squares position

--- src/util.h
+++ src/util.h
@@ -1,11 +1,12 @@
 // util.h
-// Version 1.0
+// Version 1.1
 //
 
 #ifndef PROBABLY_MONOPOLY_UTIL_H
 #define PROBABLY_MONOPOLY_UTIL_H
 
 #include <unordered_map>
+#include <unordered_set>
 #include "CircularList.h"
 
 std::unordered_map <CardsEffect, int> cardToPosition = {
@@ -17,5 +18,8 @@ std::unordered_map <CardsEffect, int> cardToPosition = {
         {TRAFALGAR_SQUARE, 13},
         {MAYFAIR, 39}
 };
+
+std::unordered_set <int> chancePosition({7, 22, 36});
+std::unordered_set <int> communityChestPosition({2, 17, 33});
 
 #endif //PROBABLY_MONOPOLY_UTIL_H
