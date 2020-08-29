0278c6d3f28e1e7c066ae1207bf1d151cba88894
andrea321123 <gal.andrea.van@gmail.com>
2020-08-29 20:42:01 +0200

    Update main()
    Implement main function for output on terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+// main.cpp
+// Version 1.0
+//
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+const int MIN_ARGUMENTS = 2;    // minimum number of arguments needed
+
+int main(int argc, char *argv[]) {
+    if (argc -1 < MIN_ARGUMENTS)
+        return 0;
+
+    // assign arguments to variables
+    int players = atoi(argv[1]);
+    int turns = atoi(argv[2]);
+
+    Board board(players);
+    SimulationResult result = board.play(turns);
+
+    std::cout << result.toString();
     return 0;
 }
